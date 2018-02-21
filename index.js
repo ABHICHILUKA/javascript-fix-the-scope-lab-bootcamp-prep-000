628f92c621f79d75ebd3f62c0ec5f27fc090ac55
Nikhil Chiluka <chnikhil058@gmail.com>
2018-02-21 17:50:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 //if(theFunk === 'FUNKY!')
-theFunk()
+var anfun = theFunk()
 
-
+anfun;
